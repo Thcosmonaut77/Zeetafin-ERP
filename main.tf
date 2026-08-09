e673ae151a54daefd9a21fc3b6805a10d6518131
Thcosmonaut77 <theactualcosmonaut@gmail.com>
2026-08-09 12:37:18 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,112 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+ 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+ 
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+ 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+ 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+ 
+ 
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 0)
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+}
+ 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+ 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+ 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+ 
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+ 
+resource "aws_security_group" "example" {
+  name   = "terraform-example-instance"
+  vpc_id = aws_vpc.main.id
+ 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+}
+ 
+resource "aws_instance" "example" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.medium"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.example.id]
+  key_name               = "stellar_key_eu-west"
+  user_data              = file("./docker.sh")
+ 
+  tags = {
+    Name = "Terraform Example Instance"
+  }
+}
+ 
+output "public_ip" {
+  description = "The public IP address of the instance"
+  value       = aws_instance.example[*].public_ip
+}
+ 
+output "private_ip" {
+  description = "The private IP address of the instance"
+  value       = aws_instance.example[*].private_ip
+}
